4800a5625828971ada532f0a5739823cd105dc04
shreyash-malani <malanishreyash24@gmail.com>
2026-02-02 16:25:56 +0530

    done

---
+++ 01-basics-react/src/App.js
@@ -0,0 +1,14 @@
+import Chai from "./chai";
+
+function App() {
+  return (
+    <>
+      <Chai/>
+
+      <h1>Chai Aur React | Basics</h1>
+    </>
+    
+  );
+}
+
+export default App;
---
+++ 01-basics-react/src/chai.js
@@ -0,0 +1,9 @@
+function Chai(){
+    return (
+        <h2>
+            Chai in react
+        </h2>
+    )
+
+}
+export default Chai 
