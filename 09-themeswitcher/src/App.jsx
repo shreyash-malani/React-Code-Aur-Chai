112a24bdcf286090b6d0e67cf234d16150170ec9
shreyash-malani <malanishreyash24@gmail.com>
2026-02-02 17:49:23 +0530

    Changes made pls add

--- 09-themeswitcher/src/App.jsx
+++ 09-themeswitcher/src/App.jsx
@@ -1,4 +1,49 @@
 
+// import { useEffect, useState } from 'react'
+// import './App.css'
+// import { ThemeProvider } from './contexts/theme'
+// import ThemeBtn from './components/ThemeBtn'
+// import Card from './components/Card'
+
+// function App() {
+//   const [themeMode, setThemeMode] = useState("light")
+
+//   const lightTheme = () => {
+//     setThemeMode("light")
+//   }
+
+//   const darkTheme = () => {
+//     setThemeMode("red")
+//   }
+
+//   // actual change in theme
+
+//   useEffect(() => {
+//     document.querySelector('html').classList.remove("light", "red")
+//     document.querySelector('html').classList.add(themeMode)
+//   }, [themeMode])
+  
+
+//   return (
+//     <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+//       <div className="flex flex-wrap min-h-screen items-center">
+//           <div className="w-full">
+//               <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+//                   <ThemeBtn />
+//               </div>
+
+//               <div className="w-full max-w-sm mx-auto">
+//                   <Card themeMode={themeMode}/>
+//               </div>
+//           </div>
+//       </div>
+//     </ThemeProvider>
+//   )
+// }
+
+// export default App
+
+
 import { useEffect, useState } from 'react'
 import './App.css'
 import { ThemeProvider } from './contexts/theme'
@@ -13,13 +58,13 @@ function App() {
   }
 
   const darkTheme = () => {
-    setThemeMode("red")
+    setThemeMode("dark")
   }
 
   // actual change in theme
 
   useEffect(() => {
-    document.querySelector('html').classList.remove("light", "red")
+    document.querySelector('html').classList.remove("light", "dark")
     document.querySelector('html').classList.add(themeMode)
   }, [themeMode])
   
@@ -33,7 +78,7 @@ function App() {
               </div>
 
               <div className="w-full max-w-sm mx-auto">
-                  <Card themeMode={themeMode}/>
+                  <Card />
               </div>
           </div>
       </div>
